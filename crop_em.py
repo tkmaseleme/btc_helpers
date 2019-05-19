bef870ff141cde5ebc65195104b5b97b54b5cafd
Amira <anazri@hotmail.com>
2019-05-19 16:14:05 +0200

    Updates

--- crop_em.py
+++ crop_em.py
@@ -9,23 +9,16 @@ with open('picture_names.csv', 'rb') as datum:
         username = row[1] + '.jpg'
         if os.path.exists(name):
             os.rename(name, username)
+        else:
+            print(name + "Does not exist")
 
-'''
-from PIL import Image
- 
 def crop(image_path, coords, saved_location):
-    """
-    @param image_path: The path to the image to edit
-    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
-    @param saved_location: Path to save the cropped image
-    """
     image_obj = Image.open(image_path)
     cropped_image = image_obj.crop(coords)
     cropped_image.save(saved_location)
     cropped_image.show()
- 
- 
+
+
 if __name__ == '__main__':
     image = 'grasshopper.jpg'
     crop(image, (161, 166, 706, 1050), 'cropped.jpg')
-'''
